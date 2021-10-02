ca4c0872a12b3afaa6b19d8f48b2a31ddf35a819
Jaime RG <75501289+Jaimrg@users.noreply.github.com>
2021-10-02 13:14:59 +0200

    CRUD Completo

--- model/Entidade/vaga.php
+++ model/Entidade/vaga.php
@@ -3,7 +3,7 @@
 namespace App\Entidade;
 
 use App\Db\Database;
-
+use \PDO;
 
 class Vaga{
 
@@ -54,8 +54,52 @@ class Vaga{
                                       'ativo'     => $this->ativo,
                                       'data'      => $this->data
                                     ]);
-     echo "<pre>" ; print_r($this); echo"</pre>"; exit;                              
+//     echo "<pre>" ; print_r($this); echo"</pre>"; exit;                              
     //RETORNAR SUCESSO
-   // return true;
+   return true;
+  }
+
+
+  /**
+   * Método responsável por atualizar a vaga no banco
+   * @return boolean
+   */
+  public function atualizar(){
+    return (new Database('vagas'))->update('id = '.$this->id,[
+                                                                'titulo'    => $this->titulo,
+                                                                'descricao' => $this->descricao,
+                                                                'ativo'     => $this->ativo,
+                                                                'data'      => $this->data
+                                                              ]);
+  }
+
+  /**
+   * Método responsável por excluir a vaga do banco
+   * @return boolean
+   */
+  public function excluir(){
+    return (new Database('vagas'))->delete('id = '.$this->id);
+  }
+
+  /**
+   * Método responsável por obter as vagas do banco de dados
+   * @param  string $where
+   * @param  string $order
+   * @param  string $limit
+   * @return array
+   */
+  public static function getVagas($where = null, $order = null, $limit = null){
+    return (new Database('vagas'))->select($where,$order,$limit)
+                                  ->fetchAll(PDO::FETCH_CLASS,self::class);
+  }
+
+  /**
+   * Método responsável por buscar uma vaga com base em seu ID
+   * @param  integer $id
+   * @return Vaga
+   */
+  public static function getVaga($id){
+    return (new Database('vagas'))->select('id = '.$id)
+                                  ->fetchObject(self::class);
   }
 }
